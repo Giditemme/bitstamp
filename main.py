464f7219fa7b03d33e33935e719d52e328654b2a
DavideRossetto99 <davide.rossetto@student.h-farm.com>
2022-01-05 16:51:02 +0100

    Commented the argparse functionality

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
 from get import create_table, get_price, get_volume, get_change
 import argparse
 
+'''
+This is the main file where we created
+the interaction between users and the
+bitstamp API.
+We used argparse module to configure
+positional and optional arguments.
+
+There are two positional argument:
+crypto and currency which reprepsent
+respectively cryptocurrency and currency
+selected by the user
+
+Additionally, the user can specify 
+which information to display, such as
+last price, daily change, volume or
+hourly chart
+'''
+
 parser = argparse.ArgumentParser()
 parser.add_argument("crypto", help="Specify the cryptocurrency symbol")
 parser.add_argument("currency", help="Specify the 3 digit currency code")
