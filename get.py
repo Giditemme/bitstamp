75cf0529b19c42cb08023460e2375382cb841202
DavideRossetto99 <davide.rossetto@student.h-farm.com>
2022-01-05 16:40:59 +0100

    Minor fixes and checked the program works

--- get.py
+++ get.py
@@ -6,21 +6,23 @@ import pandas as pd
 bitstamp_URL = 'https://www.bitstamp.net/api/v2/ticker/%s/'
 
 
-'''This function creates a csv file to fetch 
-data from bitstamp API and store it in a table'''
+def create_table(value):
 
-
-def create_table(value):	
-	r = requests.get(bitstamp_URL % value)
-	file_json = json.loads(r.text)
-	df = pd.DataFrame(file_json, index=[0])
-	df.to_csv (r'CryptoTable.csv', index = False, header=True)
+    '''
+    This function creates a csv file to fetch 
+    data from bitstamp API and store it in a table
+    '''
+    r = requests.get(bitstamp_URL % value)
+    file_json = json.loads(r.text)
+    df = pd.DataFrame(file_json, index=[0])
+    df.to_csv (r'CryptoTable.csv', index = False, header=True)
 
 def get_price():
 
-'''This function returns the variable last_price, 
-which is the last price of the cryptocurrency selected'''
-
+    '''
+    This function returns the variable last_price, 
+    which is the last price of the cryptocurrency selected
+    '''
     df = pd.read_csv('CryptoTable.csv')
     last_price = df._get_value(0, 'last')
     return last_price
@@ -28,8 +30,10 @@ which is the last price of the cryptocurrency selected'''
 
 def get_volume():
 
-'''This function returns the  volume which is the daily 
-volume of the cryptocurrency selected'''
+    '''
+    This function returns the  volume which is the daily 
+    volume of the cryptocurrency selected
+    '''
 
     df = pd.read_csv('CryptoTable.csv')
     volume = df._get_value(0, 'volume')
@@ -38,9 +42,11 @@ volume of the cryptocurrency selected'''
 
 def get_change():
 
-'''This function calculate the change in price based on 
-the opening and last price available for the 
-cryptocurrency'''
+    '''
+    This function calculate the change in price based on 
+    the opening and last price available for the 
+    cryptocurrency
+    '''
 
     df = pd.read_csv('CryptoTable.csv')
     open_price = df._get_value(0, 'open')
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from get import create_table
+from get import create_table, get_price, get_volume, get_change
 
 
-create_table(“btcusd”)
+create_table('btcusd')
+
+print(get_price(), get_volume(), get_change())
