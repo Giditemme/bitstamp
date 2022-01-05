595cc090ea20637b6ba48ae6e98c1a8d77134579
DavideRossetto99 <davide.rossetto@student.h-farm.com>
2022-01-05 16:46:00 +0100

    Initialised argparse

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 from get import create_table, get_price, get_volume, get_change
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("crypto")
+parser.add_argument("currency")
+parser.add_argument("-sd","--specific_data")
+args = parser.parse_args()
+print(args.crypto, args.currency, args.specific_data)
 
 
 create_table('btcusd')
