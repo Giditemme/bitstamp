43f4de548563d1623e0c6eef43d599420acbf23a
ginevrabarbieri <ginevra.barbieri@student.h-farm.com>
2022-01-05 17:44:45 +0100

    Functions with argparse integrated into main

--- main.py
+++ main.py
@@ -20,13 +20,45 @@ hourly chart
 '''
 
 parser = argparse.ArgumentParser()
-parser.add_argument("crypto", help="Specify the cryptocurrency symbol")
-parser.add_argument("currency", help="Specify the 3 digit currency code")
-parser.add_argument("-sd","--specific_data", help="Specify which information you want to know")
+
+parser.add_argument("crypto", help = "Specify the cryptocurrency symbol")
+parser.add_argument("currency", help = "Specify the 3 digit currency code")
+parser.add_argument("-sd","--specific_data", help = "Specify which information you want to know")
+
 args = parser.parse_args()
-print(args.crypto, args.currency, args.specific_data)
+
+value = args.crypto + args.currency # Concatenate value for API
+
+create_table(value)
+
+if args.specific_data == "price":
+    print("{} value in {} is {}".format(args.crypto, args.currency, get_price()))
+elif args.specific_data == "volume":
+    print("{} 24h volume is {}".format(args.crypto, get_volume()))
+elif args.specific_data == "change":
+    print("{} daily change is {} %".format(args.crypto, get_change()))
+else:
+    print("{} value in {} is {}".format(args.crypto, args.currency, get_price()))
+    print("{} 24h volume is {}".format(args.crypto, get_volume()))
+    print("{} daily change is {} %".format(args.crypto, get_change()))
 
 
-create_table('btcusd')
 
-print(get_price(), get_volume(), get_change())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
