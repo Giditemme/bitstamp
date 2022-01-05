da8897446b79eb7af40acdf6db1a07779b572c50
calzavarateres <teresa.calzavara@student.h-farm.com>
2022-01-05 15:20:26 +0100

    insert docstring of 3 functions

--- get.py
+++ get.py
@@ -17,16 +17,31 @@ def create_table(value):
 	df.to_csv (r'CryptoTable.csv', index = False, header=True)
 
 def get_price():
+
+'''This function returns the variable last_price, 
+which is the last price of the cryptocurrency selected'''
+
     df = pd.read_csv('CryptoTable.csv')
     last_price = df._get_value(0, 'last')
     return last_price
 
+
 def get_volume():
+
+'''This function returns the  volume which is the daily 
+volume of the cryptocurrency selected'''
+
     df = pd.read_csv('CryptoTable.csv')
     volume = df._get_value(0, 'volume')
     return volume
 
+
 def get_change():
+
+'''This function calculate the change in price based on 
+the opening and last price available for the 
+cryptocurrency'''
+
     df = pd.read_csv('CryptoTable.csv')
     open_price = df._get_value(0, 'open')
     last_price = df._get_value(0, 'last')
