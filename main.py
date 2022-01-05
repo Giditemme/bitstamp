b9a20e614ac1462ad649ba069a73459ae5023c47
DavideRossetto99 <davide.rossetto@student.h-farm.com>
2022-01-05 16:46:57 +0100

    Added help in each argument

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ from get import create_table, get_price, get_volume, get_change
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument("crypto")
-parser.add_argument("currency")
-parser.add_argument("-sd","--specific_data")
+parser.add_argument("crypto", help="Specify the cryptocurrency symbol")
+parser.add_argument("currency", help="Specify the 3 digit currency code")
+parser.add_argument("-sd","--specific_data", help="Specify which information you want to know")
 args = parser.parse_args()
 print(args.crypto, args.currency, args.specific_data)
 
