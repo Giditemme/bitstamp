a41fbf57229cc296e0fe0b7461dc8ec7606c0ef2
mattiacostacurta <mattia.costacurta@student.h-farm.com>
2022-01-05 10:09:30 +0100

    I've added a docstring

--- get.py
+++ get.py
@@ -6,6 +6,10 @@ import pandas as pd
 bitstamp_URL = 'https://www.bitstamp.net/api/v2/ticker/%s/'
 
 
+'''This function creates a csv file to fetch 
+data from bitstamp API and store it in a table'''
+
+
 def create_table(value):	
 	r = requests.get(bitstamp_URL % value)
 	file_json = json.loads(r.text)
