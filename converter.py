061c49eaa4eb53506e1b16ef857557a34d933389
calzavarateres <teresa.calzavara@student.h-farm.com>
2022-01-07 00:28:53 +0100

    Added new function for conversion

---
+++ converter.py
@@ -0,0 +1,6 @@
+def conversion(price, default_currency, output_currency):
+    c = CurrencyConverter()
+    default_currency = default_currency.upper() 
+    output_currency = output_currency.upper()
+    converted_price = c.convert(price, default_currency, output_currency)
+    return converted_price
